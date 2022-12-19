d33624fb5d85ba9246c73bea8934663d60f06596
woytekn <wojnarto@gmail.com>
2022-12-19 13:55:08 +0100

    Description for negative visual comparison added

--- e2e/tests/visual/visualComparison.spec.js
+++ e2e/tests/visual/visualComparison.spec.js
@@ -2,6 +2,11 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
 import { ProductsPage } from '../../pages/ProductsPage';
 
+/*
+If you want to check how the visual comparison failure works, just uncomment the below line. It will change the viewport, so that screens will not match.
+ test.use({ viewport: { width: 600, height: 900 } });
+*/
+
 test.beforeEach(async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goTo('inventory.html');
@@ -11,14 +16,11 @@ test('As a user I want to compare the specific element on the landing page using
   page,
 }) => {
   const productsPage = new ProductsPage(page);
-  await productsPage.productsSortingForLoop();
   await expect(productsPage.footerRobot).toHaveScreenshot('robot.png');
 });
 
 test('As a user I want to compare the state of the landing page using visual comparison', async ({
   page,
 }) => {
-  const productsPage = new ProductsPage(page);
-  await productsPage.productsSortingForLoop();
   await expect(page).toHaveScreenshot('landing_page.png');
 });
